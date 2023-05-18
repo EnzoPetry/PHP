bb262ff4fe3e8f8a5e26d6b8f6164d69b154b0e6
Enzo Petry <54451768+EnzoPetry@users.noreply.github.com>
2023-05-18 17:36:42 -0300

    Adição Form e BD

--- Form.php
+++ Form.php
@@ -1,2 +1,30 @@
+Please login:<br>
+<form method="POST">
+    <input type="text" name="user">
+    <input type="password" name="passwd">
+    <input type="submit" name="login"value="Log-in">
+</form>
 <?php
+session_start();
+include "BD.php";
+
+if (isset($_POST['user'])) {
+    $username = $_REQUEST['user'];
+}
+if (isset($_POST['passwd'])) {
+    $password = $_REQUEST['passwd'];
+}
+if (isset($_POST['login'])) {
+    $sql = "INSERT INTO users (user_name, password) VALUES ($username, $password)";
+if (mysqli_query($conn, $sql)) {
+    echo "New record created successfully";
+    session_unset();
+    header("Location: Login.php"); // Redireciona para a página de login após o logout
+    exit();
+} else {
+    echo "Error: " . $sql . "<br>" . mysqli_error($conn);
+}
+mysqli_close($conn);
+}
+
 ?>
--- Login.php
+++ Login.php
@@ -1,5 +1,6 @@
 <?php
 session_start();
+include "BD.php";
 
 if (!isset($_SESSION['visits'])) {
     $_SESSION['visits'] = 0;
@@ -11,42 +12,55 @@ if (isset($_SESSION['auth'])) {
     $auth = TRUE;
     header("Location: Teste.php"); // Redireciona para a página de login após o logout
     exit();
-} else {
-    /* Check the request string for user and password */
-    $user = '';
-    $passwd = '';
+} 
+$user = '';
+$passwd = '';
 
-    if (isset($_POST['user'])) {
-        $user = $_REQUEST['user'];
-    }
-
-    if (isset($_POST['passwd'])) {
-        $passwd = $_REQUEST['passwd'];
-    }
-
-    /* Example authentication */
-    if (($user == '1') && ($passwd == '1')) {
-        $auth = TRUE;
-        /* Save the authorized state in the Session array */
-        $_SESSION['auth'] = TRUE;
-        header("Location: Teste.php"); // Redireciona para a página de login após o logout
-        exit();
-    }
-    if (isset($_POST['signin'])) {
-        header("Location: Form.php"); // Redireciona para a página de login após o logout
-        exit();
-    }
+if (isset($_POST['user'])) {
+    $username = $_REQUEST['user'];
 }
+if (isset($_POST['passwd'])) {
+    $password = $_REQUEST['passwd'];
+}
+    /* Check the request string for user and password */
+
+
+        $sql = "SELECT * FROM users WHERE user_name = $user AND password = $passwd";
+        if (mysqli_query($conn, $sql)) {
+            echo "New record created successfully";
+            session_unset();
+            header("Location: Login.php"); // Redireciona para a página de login após o logout
+            exit();
+        } else {
+            echo "Error: " . $sql . "<br>" . mysqli_error($conn);
+        }
+        mysqli_close($conn);
+        
+
+        /* Example authentication */
+        if ($stmt->rowsCount() > 0) {
+            echo "Senha Certa";
+
+            $auth = TRUE;
+            $_SESSION['auth'] = TRUE;
+            header("Location: Teste.php"); // Redireciona para a página de login após o logout
+            exit();
+
+        } else {
+            echo "Senha Errada";
+        }
+
+
 if (!$auth) {
     ?>
 
-        Please login:<br>
-        <form method="POST">
-            <input type="text" name="user">
-            <input type="password" name="passwd">
-            <input type="submit" value="Log-in">
-            <input type="submit" name="signin" value="Sign-in">
-        </form>
-        <?php
+    Please login:<br>
+    <form method="POST">
+        <input type="text" name="user">
+        <input type="password" name="passwd">
+        <input type="submit" name="login" value="Log-in">
+        <input type="submit" value="Sign-in">
+    </form>
+    <?php
 }
 ?>
